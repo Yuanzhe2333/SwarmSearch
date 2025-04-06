b23e940bfe86d303b712872c33ceba820f3c71c9
Oscar Zhang <oscarzhang228@gmail.com>
2025-04-06 10:34:56 -0400

    implement Runnable interface for crawler

--- src/crawler/src/main/java/gt/crawler/App.java
+++ src/crawler/src/main/java/gt/crawler/App.java
@@ -4,13 +4,12 @@ import org.bson.Document;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Starting Crawler");
-
-        MongoClient mc = MongoClient.getInstance();
-
-        Crawler c1 = new Crawler();
-        c1.startCrawler("https://gatech.edu", 2);
-
-        mc.close();
+        String[] seedUrls = {"https://www.cc.gatech.edu/", "https://coe.gatech.edu/", "https://cos.gatech.edu/", "https://design.gatech.edu/", "https://iac.gatech.edu/", "https://www.scheller.gatech.edu/index.html", "https://lifetimelearning.gatech.edu/"};
+            
+        for (int i = 0; i < seedUrls.length; i++) {
+            Crawler c = new Crawler(seedUrls[i], 5);
+            new Thread(c).start();
+            System.out.println("Thread for seed url " + seedUrls[i] + " started.");
+        }
     }
 }
