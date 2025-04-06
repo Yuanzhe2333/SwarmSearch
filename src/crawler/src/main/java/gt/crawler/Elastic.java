3cabcb37ce055a6af69b36b5c9e5f8b411ca107a
tracyguo <tracyyguo15@gmail.com>
2025-04-05 22:21:18 -0400

    set up elastic class and mongodb

---
+++ src/crawler/src/main/java/gt/crawler/Elastic.java
@@ -0,0 +1,14 @@
+package gt.crawler;
+
+
+public class Elastic {
+    public Elastic() {
+        // Constructor logic here
+    }
+
+    public static void main(String[] args) {
+        // Example usage
+        Elastic elastic = new Elastic();
+        // Add your logic here
+    }
+}
